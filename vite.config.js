e812e6ff9b670fc920f6006491cbdd8bb5a669bd
Trydrica <poulin.romain@pm.men>
2026-03-28 18:25:32 +0100

    fix: update base path in Vite configuration to root for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/calculateur-perfadom/", // ← remplace par le nom exact de ton repo GitHub
+  base: "/",
 });
